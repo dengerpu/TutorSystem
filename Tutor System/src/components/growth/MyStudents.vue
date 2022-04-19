54caea7117c60b87c9ce63292c6a12a0e6af2842
dep <1649743146@qq.com>
2022-04-19 11:29:04 +0800

    4.19

--- Tutor System/src/components/growth/MyStudents.vue
+++ Tutor System/src/components/growth/MyStudents.vue
@@ -59,27 +59,30 @@
         title="制定培养计划 !"
         :before-close="handleClose"
         :visible.sync="dialog"
-        size="70%"
+        size="45%"
         direction="ltr"
         custom-class="demo-drawer"
         ref="drawer"
         >
         <div class="demo-drawer__content">
             <div class="block">
-            <span class="demonstration">折叠展示Tag</span>
-            <el-cascader
-                :options="options"
-                :props="props"
-                collapse-tags
-                clearable></el-cascader>
+                <el-select class="selectbox"  @change="getData()" v-model="optionsValue" placeholder="请选择">
+                <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                    >
+                </el-option>
+                </el-select>
+                <el-transfer v-model="value" :data="data" @change="getInfo()"></el-transfer>
             </div>
             <div class="demo-drawer__footer">
             <el-button @click="cancelForm">取 消</el-button>
-            <el-button type="primary" @click="$refs.drawer.closeDrawer()" :loading="loading">{{ loading ? '提交中 ...' : '确 定' }}</el-button>
+            <el-button type="primary" @click="$refs.drawer.closeDrawer()" >确 定</el-button>
             </div>
         </div>
         </el-drawer>
-
     </div>
 </template>
 
@@ -106,58 +109,130 @@ export default {
             total:0,  //总数据数
             studentlist: [],//用户数据
             dialog: false,
-            props: { multiple: true },
+            optionsValue:'',
+            data:[{key:"暂无",value:"暂无"}],
+            value: [],
+            plan:'',
             options: [{
-            value: 1,
-            label: '东南',
-            children: [{
-                value: 2,
-                label: '上海',
-                children: [
-                { value: 3, label: '普陀' },
-                { value: 4, label: '黄埔' },
-                { value: 5, label: '徐汇' }
-                ]
+            value: '网络安全',
+            label: '网络安全'
             }, {
-                value: 7,
-                label: '江苏',
-                children: [
-                { value: 8, label: '南京' },
-                { value: 9, label: '苏州' },
-                { value: 10, label: '无锡' }
-                ]
+            value: '软件开发',
+            label: '软件开发'
             }, {
-                value: 12,
-                label: '浙江',
-                children: [
-                { value: 13, label: '杭州' },
-                { value: 14, label: '宁波' },
-                { value: 15, label: '嘉兴' }
-                ]
+            value: '人工智能',
+            label: '人工智能'
+            }],
+            options1:[
+            {
+            key: '网络基础',
+            value: '网络基础'
+            }, {
+            key: 'windows服务安全',
+            value: 'windows服务安全'
+            }, {
+            key: 'windows安全基线',
+            value: 'windows安全基线'
+            }, {
+            key: '以太网交换与路由技术',
+            value: '以太网交换与路由技术'
+            }, {
+            key: '高级网络技术',
+            value: '高级网络技术'
+            }, {
+            key: '网络安全基线',
+            value: '网络安全基线'
+            }, {
+            key: '安全设备防护',
+            value: '安全设备防护'
+            }, {
+            key: 'Linux安全运维',
+            value: 'Linux安全运维'
+            }, {
+            key: '代码安全',
+            value: '代码安全'
+            },, {
+            key: '渗透测试',
+            value: '渗透测试'
+            }, {
+            key: '安全素养',
+            value: '安全素养'
+            }],
+            options2:[
+            {
+            key: 'HTML',
+            value: 'HTML'
+            }, {
+            key: 'CSS',
+            value: 'CSS'
+            }, {
+            key: 'JavaScript',
+            value: 'JavaScript'
+            }, {
+            key: 'Flex伸缩布局',
+            value: 'Flex伸缩布局'
+            }, {
+            key: '移动web开发',
+            value: '移动web开发'
+            }, {
+            key: 'jQuery快速开发',
+            value: 'jQuery快速开发'
+            }, {
+            key: 'JavaScript高级',
+            value: 'JavaScript高级'
+            }, {
+            key: '前后端交互Node+Culp',
+            value: '前后端交互Node+Culp'
+            }, {
+            key: '前后端交互MongoDB+Express',
+            value: '前后端交互MongoDB+Express'
+            }, {
+            key: '前后端交互Ajax',
+            value: '前后端交互Ajax'
+            },, {
+            key: '前后端交互Git和GitHub',
+            value: '前后端交互Git和GitHub'
+            }, {
+            key: 'Vue基础',
+            value: 'Vue基础'
+            }, {
+            key: '微信小程序',
+            value: '微信小程序'
+            }, {
+            key: 'React',
+            value: 'React'
+            }, {
+            key: 'ReactNative',
+            value: 'ReactNative'
+            }, {
+            key: 'Java基础',
+            value: 'Java基础'
+            },
+            {
+            key: 'JavaWeb',
+            value: 'JavaWeb'
+            }, {
+            key: 'Mybatis',
+            value: 'Mybatis'
+            }, {
+            key: 'Spring',
+            value: 'Spring'
+            }, {
+            key: 'SpringMVC',
+            value: 'SpringMVC'
+            }, {
+            key: 'Oracle',
+            value: 'Oracle'
+            }, {
+            key: 'Maven高级',
+            value: 'Maven高级'
+            }, {
+            key: 'SpringBoot',
+            value: 'SpringBoot'
             }]
-            }, {
-            value: 17,
-            label: '西北',
-            children: [{
-                value: 18,
-                label: '陕西',
-                children: [
-                { value: 19, label: '西安' },
-                { value: 20, label: '延安' }
-                ]
-            }, {
-                value: 21,
-                label: '新疆维吾尔族自治区',
-                children: [
-                { value: 22, label: '乌鲁木齐' },
-                { value: 23, label: '克拉玛依' }
-                ]
-            }]
-            }]
+        };
             
-        }
-        
-    },
+        },
     methods:{
       async getStudentList(){
             //发送请求获取数据
@@ -170,6 +245,26 @@ export default {
             }else if(res.status==200){
                 this.studentlist = res.data.list;
                 this.total = res.data.totalCount;
+            }
+        },
+        getInfo(){
+            for(let i = 0;i<this.value.length;i++){
+                if(i!=this.value.length-1){
+                    this.plan += this.value[i]+'/';
+                }else{
+                    this.plan +=this.value[i];
+                }
+            }
+            console.log(this.plan);
+        },
+        //获取内容
+        getData(){
+             if(this.optionsValue=="网络安全"){
+                this.data = this.options1;
+            }else if(this.optionsValue=="软件开发"){
+                this.data = this.options2;
+            }else{
+                this.data = [{key:"暂无",value:"暂无"}];
             }
         },
         //监听oagesize改变的事件
@@ -187,40 +282,30 @@ export default {
              this.getStudentList();
         },
          handleClose(done) {
-            if (this.loading) {
-                return;
-            }
-            this.$confirm('确定要提交表单吗？')
+            this.$confirm('确定要提交培养计划吗？')
                 .then(_ => {
-                this.loading = true;
-                this.timer = setTimeout(() => {
-                    done();
-                    // 动画关闭需要一定的时间
-                    setTimeout(() => {
-                    this.loading = false;
-                    }, 400);
-                }, 2000);
+                    console.log("提交");
+                    this.dialog = false;
                 })
-                .catch(_ => {});
+                .catch(_ => {
+                    console.log("异常");
+                });
             },
             cancelForm() {
-            this.loading = false;
-            this.dialog = false;
-            clearTimeout(this.timer);
+                this.dialog = false;
             }
     }
 }
 </script>
 
 <style lang="less" scoped>
-.el-checkbox__input{
-    display: inline-block;
+.demo-drawer__content{
+    margin-left: 50px;
 }
-.el-checkbox__label{
-    display: inline-block;
+.selectbox{
+   margin: 10px 0;
 }
-.el-transfer-panel__item{
-    background-color: red;
+.demo-drawer__footer{
+    margin: 10px 0 10px 33%;
 }
-
 </style>
